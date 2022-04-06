184309447f2abf872b90d5272195c2f524c1c7a4
martinsph <ph.mmartins@gmail.com>
2022-04-06 12:56:59 -0300

    add req 1

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,83 @@
+import csv
+
+
+def most_ordered_per_person(data, person):
+    most_order_dict = {}
+    order_counter = 0
+    most_ordered = 'pizza'
+
+    for order in data:
+        if order[0] == person:
+            if order[1] in most_order_dict:
+                most_order_dict[order[1]] += 1
+            else:
+                most_order_dict[order[1]] = 1
+
+        if most_order_dict[order[1]] > order_counter:
+            most_ordered = order[1]
+
+    # print(most_ordered)
+    return most_ordered
+
+
+def never_ordered_per_person(data, person):
+    menu = set()
+    person_orders = set()
+
+    for order in data:
+        menu.add(order[1])
+
+    for order in data:
+        if order[0] == person:
+            person_orders.add(order[1])
+
+    return menu.difference(person_orders)
+
+
+def hungry_days_per_person(data, person):
+    weekdays = set()
+    full_belly_days = set()
+
+    for order in data:
+        weekdays.add(order[2])
+
+    for order in data:
+        if order[0] == person:
+            full_belly_days.add(order[2])
+
+    return weekdays.difference(full_belly_days)
+
+
+def csv_reader(path_to_file):
+    with open(path_to_file, "r") as file:
+        result = list(csv.reader(file))
+    return result
+
+
+def csv_writer(path_to_file, data):
+    with open(path_to_file, "w") as file:
+        file.write(data)
+    pass
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    if (path_to_file.endswith(".csv")) is False:
+        raise FileNotFoundError(f"Extensão inválida: {path_to_file}")
+
+    try:
+        csv_file = csv_reader(path_to_file)
+
+        most_ordered_result = most_ordered_per_person(csv_file, "maria")
+        ordered_number_result = most_ordered_per_person(csv_file, "arnaldo")
+        never_ordered_result = never_ordered_per_person(csv_file, "joao")
+        hungry_days_result = hungry_days_per_person(csv_file, "joao")
+
+        csv_writer(
+            "./data/mkt_campaign.txt",
+            f"{most_ordered_result}\n"
+            f"{ordered_number_result}\n"
+            f"{never_ordered_result}\n"
+            f"{hungry_days_result}\n",
+        )
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: {path_to_file}")
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,92 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     # aqui deve expor a quantidade de estoque
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        return self.orders.append({
+            "customer": customer,
+            "order": order,
+            "day": day
+        })
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        most_order_dict = {}
+        order_counter = 1
+        most_ordered = 'pizza'
+
+        for order in self.orders:
+            if order["customer"] == customer:
+                if order["order"] in most_order_dict:
+                    most_order_dict[order["order"]] += 1
+                else:
+                    most_order_dict[["order"]] = 1
+
+            if most_order_dict[order["order"]] > order_counter:
+                most_ordered = order["order"]
+
+        # print(most_ordered)
+        return most_ordered
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        menu = set()
+        person_orders = set()
+
+        for order in self.orders:
+            menu.add(order["order"])
+
+        for order in self.orders:
+            if order["customer"] == customer:
+                person_orders.add(order["order"])
+
+        return menu.difference(person_orders)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        weekdays = set()
+        full_belly_days = set()
+
+        for order in self.orders:
+            weekdays.add(order["order"])
+
+        for order in self.orders:
+            if order["customer"] == customer:
+                full_belly_days.add(order["order"])
+
+        return weekdays.difference(full_belly_days)
 
     def get_busiest_day(self):
-        pass
+        weekdays_dict = {}
+        counter = 0
+        most_visited = 'terça-feira'
+
+        for order in self.orders:
+            if order["day"] in weekdays_dict:
+                weekdays_dict[order["day"]] += 1
+            else:
+                weekdays_dict[["day"]] = 1
+
+            if weekdays_dict[order["day"]] > counter:
+                most_visited = order["day"]
+
+        # print(most_visited)
+        return most_visited
 
     def get_least_busy_day(self):
-        pass
+        weekdays_dict = {}
+        counter = 0
+        least_visited = 'terça-feira'
+
+        for order in self.orders:
+            if order["day"] in weekdays_dict:
+                weekdays_dict[order["day"]] += 1
+            else:
+                weekdays_dict[["day"]] = 1
+
+            if weekdays_dict[order["day"]] <= counter:
+                least_visited = order["day"]
+
+        # print(least_visited)
+        return least_visited
